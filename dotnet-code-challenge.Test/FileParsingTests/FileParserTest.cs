3214199dcfee14d13bdc6c6069e4c5ef09187c65
stevenchendan <qwe16165850@gmail.com>
2019-09-22 23:48:10 +1000

    remove hard coded testing data

--- dotnet-code-challenge.Test/FileParsingTests/FileParserTest.cs
+++ dotnet-code-challenge.Test/FileParsingTests/FileParserTest.cs
@@ -9,14 +9,7 @@ namespace dotnet_code_challenge.Test.FileParsingTests
 {
     public class FileParserTest
     {
-        private readonly string _xmlTestData;
-        private readonly string _jsonTestData;
 
-        public FileParserTest()
-        {
-            _xmlTestData = File.ReadAllText("../../../TestFeedData/Xml_ValidResponse.xml");
-            _jsonTestData = File.ReadAllText("../../../TestFeedData/Wolferhampton_Race1.json");
-        }
 
         //TODO implement unit test
         [Fact]
