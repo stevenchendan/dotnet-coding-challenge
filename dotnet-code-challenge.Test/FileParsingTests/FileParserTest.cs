791ea6b1e208c8fbeb2d8232e8444374b97bc5f5
stevenchendan <qwe16165850@gmail.com>
2019-09-22 23:47:07 +1000

    add test data to unit test

--- dotnet-code-challenge.Test/FileParsingTests/FileParserTest.cs
+++ dotnet-code-challenge.Test/FileParsingTests/FileParserTest.cs
@@ -9,6 +9,15 @@ namespace dotnet_code_challenge.Test.FileParsingTests
 {
     public class FileParserTest
     {
+        private readonly string _xmlTestData;
+        private readonly string _jsonTestData;
+
+        public FileParserTest()
+        {
+            _xmlTestData = File.ReadAllText("../../../TestFeedData/Xml_ValidResponse.xml");
+            _jsonTestData = File.ReadAllText("../../../TestFeedData/Wolferhampton_Race1.json");
+        }
+
         //TODO implement unit test
         [Fact]
         public void GetHorsesFromXmlFile()
